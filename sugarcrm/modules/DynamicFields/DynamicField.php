ab013f12fcd207c97198486fc53d9ac6a1b92221
rajesh2011 <rbadhe@sugarcrm.com>
2018-07-18 13:36:19 -0700

    BR-6085 (SI 80879) Backport to 8.0.2 : Calculated fields populating from related record date fields with NULL or 0000-00-00 values preventing saving of records in List View

--- sugarcrm/modules/DynamicFields/DynamicField.php
+++ sugarcrm/modules/DynamicFields/DynamicField.php
@@ -424,6 +424,34 @@ class DynamicField {
                 //Change the field to be marked as a db field instead of custom for the scope of this query
                 $fields[$name] = array_merge($field, array('source' => 'db'));
                 $hasCustomFields = true;
+                if (isset($this->bean->$name)) {
+                    if (in_array(
+                        $field['type'],
+                        array(
+                            'int', 'float', 'double', 'uint', 'ulong', 'long', 'short', 'tinyint', 'currency',
+                            'decimal')
+                    )) {
+                        if (!is_numeric($this->bean->$name)) {
+                            if ($field['required']) {
+                                $this->bean->$name = 0;
+                            } else {
+                                $this->bean->$name = null;
+                            }
+                        }
+                    }
+                    if ($field['type'] == 'bool') {
+                        if ($this->bean->$name === false) {
+                            $this->bean->$name = '0';
+                        } elseif ($this->bean->$name === true) {
+                            $this->bean->$name = '1';
+                        }
+                    }
+
+                    if (($field['type'] == 'date' || $field['type'] == 'datetimecombo')
+                        && (empty($this->bean->$name) || $this->bean->$name == '1900-01-01')) {
+                        $this->bean->$name = ''; // do not set it to string 'NULL'
+                    }
+                }
                 $values[$name] = $this->bean->$name;
             }
             if (!$hasCustomFields) {
